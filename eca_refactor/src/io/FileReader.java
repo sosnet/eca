2343a9494277299d3442ad85f5606069cf486ca9
Bernd Prünster <bernd.pruenster@gmail.com>
2013-06-29 15:34:37 +0200

    parse matlab output

--- eca_refactor/src/io/FileReader.java
+++ eca_refactor/src/io/FileReader.java
@@ -7,59 +7,74 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+import rest.ResultParser;
+
 import datatypes.LambdaMatrix;
+import datatypes.PointSet;
 
 public class FileReader {
 
   private static FileReader instance;
-  
-  private FileReader() {};
-  
-  public static FileReader getInstance(){
+
+  private FileReader() {
+  };
+
+  public static FileReader getInstance() {
     if (instance == null) {
       instance = new FileReader();
     }
     return instance;
   }
-  
-  public Set<LambdaMatrix> readLambdaMatrix (String path) throws Exception {
+
+  public Set<LambdaMatrix> readLambdaMatrix(String path) throws Exception {
     Set<LambdaMatrix> buffer = new HashSet<>();
-    
+
     java.io.FileReader fr = new java.io.FileReader(path);
     BufferedReader bfr = new BufferedReader(fr);
-    
-    for(;;){
+
+    for (;;) {
       try {
         buffer.add(readMatrix(bfr));
-      }
-      catch (Exception e) {
+      } catch (Exception e) {
         bfr.close();
         return buffer;
       }
     }
   }
-  
-  private LambdaMatrix readMatrix (BufferedReader br) throws Exception {
+
+  public Set<PointSet> readPointSet(String path) throws Exception {
+    Set<PointSet> buffer = new HashSet<>();
+
+    java.io.FileReader fr = new java.io.FileReader(path);
+    BufferedReader bfr = new BufferedReader(fr);
+    String aLine;
+    while ((aLine = bfr.readLine()) != null) {
+      buffer.add(ResultParser.parsePointSet(aLine));
+    }
+    bfr.close();
+    return buffer;
+  }
+
+  private LambdaMatrix readMatrix(BufferedReader br) throws Exception {
     int gsize = Integer.parseInt(br.readLine());
     int points = Integer.parseInt(br.readLine());
-    
+
     LambdaMatrix lm = new LambdaMatrix(points, gsize);
     String matrixString = br.readLine();
-//    [-1 0 1;1 -1 0;0 1 -1]
-    StringTokenizer stRow = new StringTokenizer(matrixString,"[;]");
+    // [-1 0 1;1 -1 0;0 1 -1]
+    StringTokenizer stRow = new StringTokenizer(matrixString, "[;]");
     for (int i = 0; i < points; i++) {
       String row = stRow.nextToken();
-      StringTokenizer stCol = new StringTokenizer(row," ");
+      StringTokenizer stCol = new StringTokenizer(row, " ");
       for (int j = 0; j < points; j++) {
         lm.set(i, j, Integer.parseInt(stCol.nextToken()));
       }
     }
-    
-    br.readLine(); //empty line in input file
-    
+
+    br.readLine(); // empty line in input file
+
     return lm;
-    
-    
+
   }
-  
+
 }
--- eca_refactor/src/rest/ResultParser.java
+++ eca_refactor/src/rest/ResultParser.java
@@ -1,13 +1,15 @@
 package rest;
 
+import datatypes.GridPoint;
 import datatypes.InvalidPointSetException;
 import datatypes.PointProps;
+import datatypes.PointSet;
 
 public abstract class ResultParser {
 
-  public static PointProps parse(String props)
+  public static PointProps parseProps(String props)
       throws InvalidPointSetException {
-  //  System.out.println(props);
+     System.out.println(props);
     if (!props.contains("_"))
       throw new InvalidPointSetException("Invalid Point Set!");
     String[] result = props.split("_");
@@ -18,6 +20,21 @@ public abstract class ResultParser {
         Integer.parseInt(result[7]), Integer.parseInt(result[8]),
         Integer.parseInt(result[9]), Integer.parseInt(result[10]),
         Integer.parseInt(result[11]), Integer.parseInt(result[12]));
+
+  }
+
+  public static PointSet parsePointSet(String pointset)
+      throws InvalidPointSetException {
+    // System.out.println(props);
+    if (!pointset.contains(","))
+      throw new InvalidPointSetException("Invalid Point Set!");
+    String[] result = pointset.split(",");
+    GridPoint[] points = new GridPoint[result.length / 2];
+    for (int i = 0; i < result.length; i += 2) {
+      points[i / 2] = new GridPoint(i / 2, Integer.parseInt(result[i]),
+          Integer.parseInt(result[i + 1]));
+    }
+    return new PointSet(points, null);
 
   }
 }
